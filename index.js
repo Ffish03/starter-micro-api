15e504a06675b4bbfff02a956384b2c98c218094
Ffish03 <86238609+Ffish03@users.noreply.github.com>
2023-08-13 15:30:03 +0000

    stampo req

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var http = require('http');
 http.createServer(function (req, res) {
-    console.log(`Huhhhh Just got a request at ${req.url}!`)
-    console.log('Huh')
+    console.log(`Just got a request at ${req.url}!`)
+    console.log(req)
     res.write('Yay!');
     res.end();
 }).listen(process.env.PORT || 3000);
