7f3c93e737d16dcf94189fd5546c3801c1032a53
Ffish03 <86238609+Ffish03@users.noreply.github.com>
2024-04-24 17:10:55 +0000

    tentativo 2

--- index.js
+++ index.js
@@ -1,6 +1,16 @@
 var http = require('http');
 http.createServer(function (req, res) {
     console.log(`Just got a request at ${req.url}!`)
+    if (req.method === 'POST' && req.url === '/login') {
+        let body = '';
+        req.on('data', chunk => {
+            body += chunk.toString(); // convert Buffer to string
+        });
+        req.on('end', () => {
+            console.log(body);
+            res.end('ok');
+        });
+    }
     res.write('Yay!');
     res.end();
 }).listen(process.env.PORT || 3000);
